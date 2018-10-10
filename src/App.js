9cdb1a56236401b099981240868917d53fe04b49
John Lange <lange.john.m@gmail.com>
2018-10-10 09:59:22 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import data from './apidata.js';
 
 class App extends Component {
+  constructor(props)
+  {
+    super(props)
+
+    this.state = {
+      moviedata: data
+    }
+  }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.moviedata.results.map( (movie) => {
+          return (
+           <div>
+             <h1>{movie.title}</h1>
+             <img src={"https://image.tmdb.org/t/p/w500" + movie.poster_path} />
+            </div>
+           )
+        } )}
       </div>
     );
   }
 }
 
 export default App;
+
---
+++ src/apidata.js
@@ -0,0 +1,422 @@
+const data = {
+    "page": 1,
+    "total_results": 381256,
+    "total_pages": 19063,
+    "results": [
+    {
+    "vote_count": 632,
+    "id": 335983,
+    "video": false,
+    "vote_average": 6.7,
+    "title": "Venom",
+    "popularity": 708.529,
+    "poster_path": "/2uNW4WbgBXL25BAbXGLnLqX71Sw.jpg",
+    "original_language": "en",
+    "original_title": "Venom",
+    "genre_ids": [
+    27,
+    878,
+    28,
+    53
+    ],
+    "backdrop_path": "/VuukZLgaCrho2Ar8Scl9HtV3yD.jpg",
+    "adult": false,
+    "overview": "When Eddie Brock acquires the powers of a symbiote, he will have to release his alter-ego “Venom” to save his life.",
+    "release_date": "2018-10-03"
+    },
+    {
+    "vote_count": 2543,
+    "id": 363088,
+    "video": false,
+    "vote_average": 6.9,
+    "title": "Ant-Man and the Wasp",
+    "popularity": 202.83,
+    "poster_path": "/rv1AWImgx386ULjcf62VYaW8zSt.jpg",
+    "original_language": "en",
+    "original_title": "Ant-Man and the Wasp",
+    "genre_ids": [
+    28,
+    12,
+    35,
+    878,
+    10751,
+    10749
+    ],
+    "backdrop_path": "/6P3c80EOm7BodndGBUAJHHsHKrp.jpg",
+    "adult": false,
+    "overview": "Just when his time under house arrest is about to end, Scott Lang puts again his freedom at risk to help Hope van Dyne and Dr. Hank Pym dive into the quantum realm and try to accomplish, against time and any chance of success, a very dangerous rescue mission.",
+    "release_date": "2018-07-04"
+    },
+    {
+    "vote_count": 1012,
+    "id": 439079,
+    "video": false,
+    "vote_average": 5.8,
+    "title": "The Nun",
+    "popularity": 198.607,
+    "poster_path": "/sFC1ElvoKGdHJIWRpNB3xWJ9lJA.jpg",
+    "original_language": "en",
+    "original_title": "The Nun",
+    "genre_ids": [
+    27,
+    9648,
+    53
+    ],
+    "backdrop_path": "/fgsHxz21B27hOOqQBiw9L6yWcM7.jpg",
+    "adult": false,
+    "overview": "When a young nun at a cloistered abbey in Romania takes her own life, a priest with a haunted past and a novitiate on the threshold of her final vows are sent by the Vatican to investigate. Together they uncover the order’s unholy secret. Risking not only their lives but their faith and their very souls, they confront a malevolent force in the form of the same demonic nun that first terrorized audiences in “The Conjuring 2,” as the abbey becomes a horrific battleground between the living and the damned.",
+    "release_date": "2018-09-05"
+    },
+    {
+    "vote_count": 471,
+    "id": 346910,
+    "video": false,
+    "vote_average": 5.3,
+    "title": "The Predator",
+    "popularity": 197.255,
+    "poster_path": "/wMq9kQXTeQCHUZOG4fAe5cAxyUA.jpg",
+    "original_language": "en",
+    "original_title": "The Predator",
+    "genre_ids": [
+    27,
+    878,
+    28,
+    35
+    ],
+    "backdrop_path": "/f4E0ocYeToEuXvczZv6QArrMDJ.jpg",
+    "adult": false,
+    "overview": "From the outer reaches of space to the small-town streets of suburbia, the hunt comes home. Now, the universe’s most lethal hunters are stronger, smarter and deadlier than ever before, having genetically upgraded themselves with DNA from other species. When a young boy accidentally triggers their return to Earth, only a ragtag crew of ex-soldiers and a disgruntled science teacher can prevent the end of the human race.",
+    "release_date": "2018-09-13"
+    },
+    {
+    "vote_count": 66,
+    "id": 463272,
+    "video": false,
+    "vote_average": 6.3,
+    "title": "Johnny English Strikes Again",
+    "popularity": 138.069,
+    "poster_path": "/tCBxnZwLiY1BOKw3tH6AxHZdqPh.jpg",
+    "original_language": "en",
+    "original_title": "Johnny English Strikes Again",
+    "genre_ids": [
+    12,
+    10751,
+    28,
+    35
+    ],
+    "backdrop_path": "/yCOLqh5MOGyYdo58Ap0aWvKop9h.jpg",
+    "adult": false,
+    "overview": "Disaster strikes when a criminal mastermind reveals the identities of all active undercover agents in Britain. The secret service can now rely on only one man—Johnny English. Currently teaching at a minor prep school, Johnny springs back into action to find the mysterious hacker. For this mission to succeed, he’ll need all of his skills—what few he has—as the man with yesterday’s analogue methods faces off against tomorrow’s digital technology.",
+    "release_date": "2018-09-13"
+    },
+    {
+    "vote_count": 8595,
+    "id": 299536,
+    "video": false,
+    "vote_average": 8.3,
+    "title": "Avengers: Infinity War",
+    "popularity": 137.262,
+    "poster_path": "/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg",
+    "original_language": "en",
+    "original_title": "Avengers: Infinity War",
+    "genre_ids": [
+    12,
+    878,
+    28,
+    14
+    ],
+    "backdrop_path": "/lmZFxXgJE3vgrciwuDib0N8CfQo.jpg",
+    "adult": false,
+    "overview": "As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle, a new danger has emerged from the cosmic shadows: Thanos. A despot of intergalactic infamy, his goal is to collect all six Infinity Stones, artifacts of unimaginable power, and use them to inflict his twisted will on all of reality. Everything the Avengers have fought for has led up to this moment - the fate of Earth and existence itself has never been more uncertain.",
+    "release_date": "2018-04-25"
+    },
+    {
+    "vote_count": 8447,
+    "id": 284054,
+    "video": false,
+    "vote_average": 7.4,
+    "title": "Black Panther",
+    "popularity": 109.984,
+    "poster_path": "/uxzzxijgPIY7slzFvMotPv8wjKA.jpg",
+    "original_language": "en",
+    "original_title": "Black Panther",
+    "genre_ids": [
+    28,
+    12,
+    14,
+    878
+    ],
+    "backdrop_path": "/b6ZJZHUdMEFECvGiDpJjlfUWela.jpg",
+    "adult": false,
+    "overview": "King T'Challa returns home from America to the reclusive, technologically advanced African nation of Wakanda to serve as his country's new leader. However, T'Challa soon finds that he is challenged for the throne by factions within his own country as well as without. Using powers reserved to Wakandan kings, T'Challa assumes the Black Panther mantel to join with girlfriend Nakia, the queen-mother, his princess-kid sister, members of the Dora Milaje (the Wakandan 'special forces') and an American secret agent, to prevent Wakanda from being dragged into a world war.",
+    "release_date": "2018-02-13"
+    },
+    {
+    "vote_count": 3800,
+    "id": 351286,
+    "video": false,
+    "vote_average": 6.5,
+    "title": "Jurassic World: Fallen Kingdom",
+    "popularity": 101.082,
+    "poster_path": "/c9XxwwhPHdaImA2f1WEfEsbhaFB.jpg",
+    "original_language": "en",
+    "original_title": "Jurassic World: Fallen Kingdom",
+    "genre_ids": [
+    28,
+    12,
+    878,
+    27
+    ],
+    "backdrop_path": "/3s9O5af2xWKWR5JzP2iJZpZeQQg.jpg",
+    "adult": false,
+    "overview": "Three years after the demise of Jurassic World, a volcanic eruption threatens the remaining dinosaurs on the isla Nublar, so Claire Dearing, the former park manager, recruits Owen Grady to help prevent the extinction of the dinosaurs once again.",
+    "release_date": "2018-06-06"
+    },
+    {
+    "vote_count": 228,
+    "id": 332562,
+    "video": false,
+    "vote_average": 7.3,
+    "title": "A Star Is Born",
+    "popularity": 97.178,
+    "poster_path": "/wrFpXMNBRj2PBiN4Z5kix51XaIZ.jpg",
+    "original_language": "en",
+    "original_title": "A Star Is Born",
+    "genre_ids": [
+    18,
+    10402,
+    10749
+    ],
+    "backdrop_path": "/840rbblaLc4SVxm8gF3DNdJ0YAE.jpg",
+    "adult": false,
+    "overview": "Seasoned musician Jackson Maine discovers—and falls in love with—struggling artist Ally. She has just about given up on her dream to make it big as a singer—until Jack coaxes her into the spotlight. But even as Ally's career takes off, the personal side of their relationship is breaking down, as Jack fights an ongoing battle with his own internal demons.",
+    "release_date": "2018-10-03"
+    },
+    {
+    "vote_count": 40,
+    "id": 507569,
+    "video": false,
+    "vote_average": 5.9,
+    "title": "The Seven Deadly Sins: Prisoners of the Sky",
+    "popularity": 93.224,
+    "poster_path": "/r6pPUVUKU5eIpYj4oEzidk5ZibB.jpg",
+    "original_language": "ja",
+    "original_title": "The Seven Deadly Sins: Prisioneros del Cielo",
+    "genre_ids": [
+    28,
+    12,
+    14,
+    16
+    ],
+    "backdrop_path": "/uKwOX7MtKlAaGeCQe6c4jc1vZpj.jpg",
+    "adult": false,
+    "overview": "Traveling in search of the rare ingredient, “sky fish”  Meliodas and Hawk arrive at a palace that floats above the clouds. The people there are busy preparing a ceremony, meant to protect their home from a ferocious beast that awakens once every 3,000 years. But before the ritual is complete, the Six Knights of Black—a Demon Clan army—removes the seal on the beast, threatening the lives of everyone in the Sky Palace.",
+    "release_date": "2018-08-18"
+    },
+    {
+    "vote_count": 45,
+    "id": 446894,
+    "video": false,
+    "vote_average": 7.1,
+    "title": "Smallfoot",
+    "popularity": 92.549,
+    "poster_path": "/4nKoB6wMVXfsYgRZK5lHZ5VMQ6J.jpg",
+    "original_language": "en",
+    "original_title": "Smallfoot",
+    "genre_ids": [
+    35,
+    16,
+    10751,
+    12,
+    14
+    ],
+    "backdrop_path": "/7t88SoT3Dd8DhGnQuVoSbMNUl3W.jpg",
+    "adult": false,
+    "overview": "A bright young yeti finds something he thought didn't exist—a human. News of this “smallfoot” throws the simple yeti community into an uproar over what else might be out there in the big world beyond their snowy village.",
+    "release_date": "2018-09-20"
+    },
+    {
+    "vote_count": 931,
+    "id": 447200,
+    "video": false,
+    "vote_average": 6.1,
+    "title": "Skyscraper",
+    "popularity": 92.203,
+    "poster_path": "/5LYSsOPzuP13201qSzMjNxi8FxN.jpg",
+    "original_language": "en",
+    "original_title": "Skyscraper",
+    "genre_ids": [
+    28,
+    53,
+    18
+    ],
+    "backdrop_path": "/oMKFQmoVgB69fyXfSMu0lGlHJP2.jpg",
+    "adult": false,
+    "overview": "Framed and on the run, a former FBI agent must save his family from a blazing fire in the world's tallest building.",
+    "release_date": "2018-07-11"
+    },
+    {
+    "vote_count": 760,
+    "id": 400155,
+    "video": false,
+    "vote_average": 6.8,
+    "title": "Hotel Transylvania 3: Summer Vacation",
+    "popularity": 89.757,
+    "poster_path": "/gjAFM4xhA5vyLxxKMz38ujlUfDL.jpg",
+    "original_language": "en",
+    "original_title": "Hotel Transylvania 3: Summer Vacation",
+    "genre_ids": [
+    10751,
+    14,
+    35,
+    16
+    ],
+    "backdrop_path": "/m03jul0YdVEOFXEQVUv6pOVQYGL.jpg",
+    "adult": false,
+    "overview": "Dracula, Mavis, Johnny and the rest of the Drac Pack take a vacation on a luxury Monster Cruise Ship, where Dracula falls in love with the ship’s captain, Ericka, who’s secretly a descendant of Abraham Van Helsing, the notorious monster slayer.",
+    "release_date": "2018-06-28"
+    },
+    {
+    "vote_count": 2085,
+    "id": 348350,
+    "video": false,
+    "vote_average": 6.7,
+    "title": "Solo: A Star Wars Story",
+    "popularity": 87.998,
+    "poster_path": "/3IGbjc5ZC5yxim5W0sFING2kdcz.jpg",
+    "original_language": "en",
+    "original_title": "Solo: A Star Wars Story",
+    "genre_ids": [
+    28,
+    12,
+    878,
+    80,
+    37,
+    10749
+    ],
+    "backdrop_path": "/5DUqFLgkLsJxyqPCAcgTivZy2SX.jpg",
+    "adult": false,
+    "overview": "Through a series of daring escapades deep within a dark and dangerous criminal underworld, Han Solo meets his mighty future copilot Chewbacca and encounters the notorious gambler Lando Calrissian.",
+    "release_date": "2018-05-15"
+    },
+    {
+    "vote_count": 5439,
+    "id": 383498,
+    "video": false,
+    "vote_average": 7.5,
+    "title": "Deadpool 2",
+    "popularity": 83.042,
+    "poster_path": "/to0spRl1CMDvyUbOnbb4fTk3VAd.jpg",
+    "original_language": "en",
+    "original_title": "Deadpool 2",
+    "genre_ids": [
+    28,
+    35,
+    878
+    ],
+    "backdrop_path": "/3P52oz9HPQWxcwHOwxtyrVV1LKi.jpg",
+    "adult": false,
+    "overview": "Wisecracking mercenary Deadpool battles the evil and powerful Cable and other bad guys to save a boy's life.",
+    "release_date": "2018-05-15"
+    },
+    {
+    "vote_count": 1136,
+    "id": 345940,
+    "video": false,
+    "vote_average": 6,
+    "title": "The Meg",
+    "popularity": 81.611,
+    "poster_path": "/eyWICPcxOuTcDDDbTMOZawoOn8d.jpg",
+    "original_language": "en",
+    "original_title": "The Meg",
+    "genre_ids": [
+    28,
+    878,
+    53
+    ],
+    "backdrop_path": "/rH79sB6Nkx4cMW3JzsUy7wK0rhX.jpg",
+    "adult": false,
+    "overview": "A deep sea submersible pilot revisits his past fears in the Mariana Trench, and accidentally unleashes the seventy foot ancestor of the Great White Shark believed to be extinct.",
+    "release_date": "2018-08-09"
+    },
+    {
+    "vote_count": 1804,
+    "id": 353081,
+    "video": false,
+    "vote_average": 7.3,
+    "title": "Mission: Impossible - Fallout",
+    "popularity": 77.818,
+    "poster_path": "/AkJQpZp9WoNdj7pLYSj1L0RcMMN.jpg",
+    "original_language": "en",
+    "original_title": "Mission: Impossible - Fallout",
+    "genre_ids": [
+    12,
+    28,
+    53
+    ],
+    "backdrop_path": "/a9fRqsl6P27qxbDSb4pBXNv9Fwl.jpg",
+    "adult": false,
+    "overview": "When an IMF mission ends badly, the world is faced with dire consequences. As Ethan Hunt takes it upon himself to fulfill his original briefing, the CIA begin to question his loyalty and his motives. The IMF team find themselves in a race against time, hunted by assassins while trying to prevent a global catastrophe.",
+    "release_date": "2018-07-13"
+    },
+    {
+    "vote_count": 93,
+    "id": 443463,
+    "video": false,
+    "vote_average": 7,
+    "title": "Leave No Trace",
+    "popularity": 75.457,
+    "poster_path": "/awGdHLQdPARAWyiT4hzu1ZFdsHO.jpg",
+    "original_language": "en",
+    "original_title": "Leave No Trace",
+    "genre_ids": [
+    18
+    ],
+    "backdrop_path": "/c5GHB444LQShWnO29ulpFMLo4Am.jpg",
+    "adult": false,
+    "overview": "A father and daughter live a perfect but mysterious existence in Forest Park, a beautiful nature reserve near Portland, Oregon, rarely making contact with the world. But when a small mistake tips them off to authorities, they are sent on an increasingly erratic journey in search of a place to call their own.",
+    "release_date": "2018-06-29"
+    },
+    {
+    "vote_count": 31,
+    "id": 401478,
+    "video": false,
+    "vote_average": 5.5,
+    "title": "Death Race 4: Beyond Anarchy",
+    "popularity": 71.938,
+    "poster_path": "/1PrlKvPUbDQAqFQBCyyt68hLLl.jpg",
+    "original_language": "en",
+    "original_title": "Death Race 4: Beyond Anarchy",
+    "genre_ids": [
+    28
+    ],
+    "backdrop_path": "/W8V8d9bjLZURgEGbIB5TC70yaj.jpg",
+    "adult": false,
+    "overview": "Black Ops specialist Connor Gibson infiltrates a maximum security prison to take down legendary driver Frankenstein in a violent and brutal car race.",
+    "release_date": "2018-01-30"
+    },
+    {
+    "vote_count": 968,
+    "id": 442249,
+    "video": false,
+    "vote_average": 5.8,
+    "title": "The First Purge",
+    "popularity": 71.629,
+    "poster_path": "/litjsBoiydO6JlO70uOX4N3WnNL.jpg",
+    "original_language": "en",
+    "original_title": "The First Purge",
+    "genre_ids": [
+    28,
+    878,
+    53,
+    27
+    ],
+    "backdrop_path": "/r4clWIxKeWoqrHspQP0XunuUmB7.jpg",
+    "adult": false,
+    "overview": "To push the crime rate below one percent for the rest of the year, the New Founding Fathers of America test a sociological theory that vents aggression for one night in one isolated community. But when the violence of oppressors meets the rage of the others, the contagion will explode from the trial-city borders and spread across the nation.",
+    "release_date": "2018-07-04"
+    }
+    ]
+    }
+
+    export default data;
